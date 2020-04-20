f332b81baef7ef1832e168550ef3fec263a8b94e
Pedro Rendeiro <PedroRendeiro@users.noreply.github.com>
2020-04-20 11:06:29 +0100

    Update lite.py

--- YOLOv3/lite.py
+++ YOLOv3/lite.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 from PIL import Image
 import time
 
@@ -12,7 +12,7 @@ def load_labels(filename):
         return [line.strip() for line in f.readlines()]
 
 # Load TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path="weights/emsfiiot_lite.h5")
+interpreter = tflite.Interpreter(model_path="weights/emsfiiot_lite.h5")
 interpreter.allocate_tensors()
 
 # Get input and output tensors.
@@ -37,23 +37,24 @@ print(image_data.shape)
 
 interpreter.set_tensor(input_details[0]['index'], image_data)
 
-start = time.time()
-interpreter.invoke()
-end = time.time()
+while True:
+    start = time.time()
+    interpreter.invoke()
+    end = time.time()
 
-# The function `get_tensor()` returns a copy of the tensor data.
-# Use `tensor()` in order to get a pointer to the tensor.
-output_data = [interpreter.get_tensor(output_details[2]['index']), interpreter.get_tensor(output_details[0]['index']), interpreter.get_tensor(output_details[1]['index'])]
+    # The function `get_tensor()` returns a copy of the tensor data.
+    # Use `tensor()` in order to get a pointer to the tensor.
+    output_data = [interpreter.get_tensor(output_details[2]['index']), interpreter.get_tensor(output_details[0]['index']), interpreter.get_tensor(output_details[1]['index'])]
 
-anchors = get_anchors("configs/yolo3_anchors.txt")
-class_names = get_classes("configs/emsfiiot_classes.txt")
-colors = get_colors(class_names)
-out_boxes, out_classes, out_scores = yolo3_postprocess_np(output_data, image_shape, anchors, len(class_names), model_image_size, max_boxes=20)
+    anchors = get_anchors("configs/yolo3_anchors.txt")
+    class_names = get_classes("configs/emsfiiot_classes.txt")
+    colors = get_colors(class_names)
+    out_boxes, out_classes, out_scores = yolo3_postprocess_np(output_data, image_shape, anchors, len(class_names), model_image_size, max_boxes=20)
 
-print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
-print("Inference time: {:.8f}s".format(end - start))
+    print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
+    print("Inference time: {:.8f}s".format(end - start))
 
-image_array = np.array(image, dtype='uint8')
-image_array = draw_boxes(image_array, out_boxes, out_classes, out_scores, class_names, colors)
+    image_array = np.array(image, dtype='uint8')
+    image_array = draw_boxes(image_array, out_boxes, out_classes, out_scores, class_names, colors)
 
 Image.fromarray(image_array).show()
